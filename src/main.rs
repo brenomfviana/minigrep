c420b6992ce42b09af1770260b63c434be6577a3
Breno Viana <brenomfviana@gmail.com>
2020-03-29 17:48:26 -0300

    adds command line arguments reading function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+//! This file is part of minigrep.
+//!
+//! Copyright (c) 2020 by Breno Viana.
+//!
+//! Minigrep is a free software; you can redistribute it and/or modify it under
+//! the terms of the MIT License.
+
+use std::env;
+
+/// Gets the arguments and validate them.
+fn get_args() -> (String, String) {
+  // Get arguments
+  let args: Vec<String> = env::args().collect();
+  // Check if the arguments are valid
+  match args.len() {
+    0 => panic!("ERROR: How do you did that?"),
+    1 => panic!("ERROR: You did not enter any arguments."),
+    2 => panic!("ERROR: You did not enter the filename."),
+    _ => println!("WARNING: You entered more arguments than the necessary."),
+  }
+  // Return the tuple of arguments
+  (args[1].clone(), args[2].clone())
+}
+
 fn main() {
-  println!("Hello, world!");
+  // Get query key and filename
+  let (query, filename) = get_args();
+  // Print arguments
+  println!("Searching for {}", query);
+  println!("In file {}", filename);
 }
