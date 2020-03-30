77d742545d3d41c8aabb81644a71a2a3d001e741
Breno Viana <brenomfviana@gmail.com>
2020-03-29 22:22:54 -0300

    updates error messages to be printed into standard error output

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-  println!("Minigrep");
   // Get arguments
   let args: Vec<String> = env::args().collect();
   // Get the search configuration
   let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {}", err);
+    eprintln!("Problem parsing arguments: {}", err);
     process::exit(1);
   });
+  println!("Minigrep");
   println!("  Set any value to CASE_INSENSITIVE environment variable to use \
     case insensitive search.");
   // Perform the search
   if let Err(e) = minigrep::run(config) {
-    println!("Application error: {}.", e);
+    eprintln!("Application error: {}.", e);
     process::exit(1);
   }
 }
